97f20a2096218037478f1c82576dce75ace2af6b
shun103100000 <shun103100000@gmail.com>
2026-07-28 02:18:25 +0900

    commit

---
+++ app/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// Redirect handled by routeRules in nuxt.config.ts
+// This page exists as a fallback
+definePageMeta({
+  layout: 'default',
+})
+
+</script>
+
+<template>
+  <div>Hello world!</div>
+</template>
